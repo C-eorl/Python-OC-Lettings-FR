49f82f0eaf7e3df7a517a95a60c9e4b364509c1d
ceorl <dev.rocher.florian@gmail.com>
2026-03-03 16:37:39 +0100

    - add test for index

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,8 @@
-def test_dummy():
-    assert 1
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_lettings_site_index(client):
+    response = client.get(reverse('index'))
+    assert response.status_code == 200
