1fb5aed728423d3b2729016b9c3035a5d5243389
ceorl <dev.rocher.florian@gmail.com>
2026-03-06 14:23:27 +0100

    - test CI

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ if SENTRY_DSN:
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default="unsafe-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
